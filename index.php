102c5bbcc0b5a6bf0aea8b0602662abb3b2772ca
Asad Ilyas <asadilyas.ca@gmail.com>
2022-08-13 11:29:47 +0500

    started

--- index.php
+++ index.php
@@ -9,6 +9,5 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque impedit ad, alias facere velit, ipsa adipisci libero illum animi quam doloribus, voluptatibus excepturi exercitationem distinctio. Architecto reprehenderit maiores quisquam quas!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas similique corporis dicta omnis qui? Deserunt saepe unde ex numquam at officia veniam obcaecati, natus ut? Expedita, molestias? Illo, architecto deleniti.</p>
 </body>
 </html>
