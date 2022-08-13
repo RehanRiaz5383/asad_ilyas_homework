4b863fb195e0c1bfbe4e0bde8233160574fed4e5
Asad Ilyas <asadilyas.ca@gmail.com>
2022-08-13 11:11:27 +0500

    started

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque impedit ad, alias facere velit, ipsa adipisci libero illum animi quam doloribus, voluptatibus excepturi exercitationem distinctio. Architecto reprehenderit maiores quisquam quas!</p>
+    
+</body>
+</html>
